7caa4ea9e5226fe630b3ea2120d6e2f8b7de9040
alicelindholm <54351014+alicelindholm@users.noreply.github.com>
2019-09-02 13:45:25 +0200

    Klar

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Min sida</title>
+</head>
+<body>
+<h1> Min sida </h1>
+<?php
+
+
+echo "1 elefant balanserade
+på en liten spindel-tråd
+Det tyckte han var så intressant,
+så han gick och hämtade en annan elefant<br/";
+
+for ($i = 2; $i <= 100; $i++){
+    echo $i;
+    echo " elefanter balanserade på en liten spindel-tråd. Det tyckte dom var så intressant,så dom gick och hämtade en annan elefant.<br/>";
+}
+
+?>
+</body>
+</html>
+
+
